d074f5976b4d15fde61b4503516a24442baa3f01
Czajka <mikol@10.2.8.41>
2017-04-03 00:10:45 +0200

    Add ButtonStationaryQAdd class

---
+++ ChargesSimulator/src/gui/ButtonStationaryQAdd.java
@@ -0,0 +1,33 @@
+package gui;
+
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+public class ButtonStationaryQAdd extends JButton {
+
+	public ButtonStationaryQAdd() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ButtonStationaryQAdd(Icon icon) {
+		super(icon);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ButtonStationaryQAdd(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ButtonStationaryQAdd(Action a) {
+		super(a);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ButtonStationaryQAdd(String text, Icon icon) {
+		super(text, icon);
+		// TODO Auto-generated constructor stub
+	}
+
+}
