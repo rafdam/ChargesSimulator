9b7430afed7a23aa8b9380b34ef2800c59ebff39
rafdam <rafjankowski1996@gmail.com>
2017-04-04 21:01:37 +0200

    Fully Math Model

--- ChargesSimulator/src/model/TestCharge.java
+++ ChargesSimulator/src/model/TestCharge.java
@@ -3,24 +3,43 @@ package model;
 import java.util.ArrayList;
 
 public class TestCharge {
-
+		
+		double testChargeVelocityX = 0; 
+		double testChargeVelocityY = 0;
+		double testChargeVelocityZ = 0;
+		double testChargeXCord = 0;
+		double testChargeConstant = 0;
+	
+	public double getTestChargeVelocityX(){
+		return testChargeVelocityX;
+	}
+	
+	public double getTestChargeConstant(){
+		return testChargeConstant;
+	}
+	
+	public double getTestChargeVelocityY(){
+		return testChargeVelocityY;
+	}
+	
+	public double getTestChargeVelocityZ(){
+		return testChargeVelocityZ;
+	}
 	
 	public TestCharge() {
 		// TODO Auto-generated constructor stub
 	}
 	public TestCharge(boolean a, double b, double c, double d){
-		boolean ifTestChargeSignPositive = a;
-		double testChargeMass = 0;
-		double testChargeVelocityX = b; 
-		double testChargeVelocityY = c;
-		double testChargeVelocityZ = d;
+			testChargeVelocityX = b; 
+			testChargeVelocityY = c;
+			testChargeVelocityZ = d;
 		
 		if (a == false){
-			testChargeMass = 9.10938215*Math.pow(10, -31);
+			testChargeConstant = -14.592*Math.pow(10,12);
 			}
 		
 		else{
-			testChargeMass = 1.672621637*Math.pow(10, -27);
+			testChargeConstant = 2.679538842*Math.pow(10,8);
 		}
 		
 	}
--- ChargesSimulator/src/model/TestChargeMotion.java
+++ ChargesSimulator/src/model/TestChargeMotion.java
@@ -20,6 +20,7 @@ public class TestChargeMotion extends TestCharge implements ChargesPotentialInte
 		return 0;
 		}
 
+	//TestPullcomment
 
 
 	public double getElectricForceY() {
@@ -34,15 +35,51 @@ public class TestChargeMotion extends TestCharge implements ChargesPotentialInte
 		return 0;
 	}
 	
-	public void Motion(ChargesPotential ch, TestCharge test1 ){
-		/*
-		 * Space for solving the F=ma equation
-		 */
+		double motionXCord = 0;
+		double motionYCord = 0;
+		double motionZCord = 0;
+		double motionXVelo = 0;
+		double motionYVelo = 0;
+		double motionZVelo = 0;
+		
+	public TestChargeMotion(ChargesPotential ch, TestCharge test1, double t ){
+		motionXVelo = ch.getElectricForceX() * test1.getTestChargeConstant() * t + test1.getTestChargeVelocityX();
+		motionYVelo = ch.getElectricForceY() * test1.getTestChargeConstant() * t + test1.getTestChargeVelocityY();
+		motionZVelo = ch.getElectricForceZ() * test1.getTestChargeConstant() * t + test1.getTestChargeVelocityZ();
+		motionXCord = ch.getElectricForceX() * test1.getTestChargeConstant() * Math.pow(t, 2) + test1.getTestChargeVelocityX()*t + test1.testChargeXCord;
+		motionYCord = ch.getElectricForceY() * test1.getTestChargeConstant() * Math.pow(t, 2) + test1.getTestChargeVelocityY()*t;
+		motionZCord = ch.getElectricForceZ() * test1.getTestChargeConstant() * Math.pow(t, 2) + test1.getTestChargeVelocityZ()*t;
+		}
+	
+	public double getMotionXVelo(){
+		return motionXVelo;
+	}
+	
+	public double getMotionYVelo(){
+		return motionYVelo;
+	}
+	
+	public double getMotionZVelo(){
+		return motionZVelo;
+	}
+	
+	public double getMotionXCord(){
+		return motionXCord;
+	}
+	
+	public double getMotionYCord(){
+		return motionYCord;
+	}
+	
+	public double getMotionZCord(){
+		return motionZCord;
 	}
 	
 	public static void main(String[] args) {
 	ChargesList List = new ChargesList();
 	TestPoint Point = new TestPoint(3,2,8);
+	TestCharge tCharge1 = new TestCharge(true , 0 , 0 , 0 );
+	TestCharge tCharge2 = new TestCharge(false , 0 , 0 , 0 );
 	StationaryCharge ch1 = new StationaryCharge(1,2,3,-4);
 	StationaryCharge ch2 = new StationaryCharge(5,10,3,8);
 	StationaryCharge ch3 = new StationaryCharge(8,2,5,10);
@@ -50,9 +87,15 @@ public class TestChargeMotion extends TestCharge implements ChargesPotentialInte
 	List.add(ch2);
 	List.add(ch3);	
 	ChargesPotential Potential = new ChargesPotential(List, Point);
+	//System.out.println(Potential.getElectricForceX());
+	//System.out.println(Potential.getElectricForceY());
+	//System.out.println(Potential.getElectricForceZ());
+	TestChargeMotion chargeMotion = new TestChargeMotion(Potential, tCharge1, 0.001);
+	TestChargeMotion chargeMotion1 = new TestChargeMotion(Potential, tCharge2, 0.001);
 	System.out.println(Potential.getElectricForceX());
-	System.out.println(Potential.getElectricForceY());
-	System.out.println(Potential.getElectricForceZ());
-
+	System.out.println(chargeMotion.getMotionXCord());
+	System.out.println(chargeMotion1.getMotionXCord());
+	System.out.println(tCharge1.getTestChargeConstant());
+	System.out.println(tCharge2.getTestChargeConstant());
 	}
 }
