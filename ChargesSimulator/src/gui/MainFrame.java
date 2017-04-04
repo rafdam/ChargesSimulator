ed0f8dbae8f000a21454fcb518fc8e7cf73466a6
Czajka <mikol@10.2.8.41>
2017-04-04 15:21:33 +0200

    First gui commit

---
+++ ChargesSimulator/src/gui/MainFrame.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public MainFrame() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainFrame(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainFrame(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainFrame(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
