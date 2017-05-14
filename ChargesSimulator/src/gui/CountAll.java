d7789a197f40ffd877810a57ef135a1ec5d0bae9
rafdam <rafjankowski1996@gmail.com>
2017-05-14 19:59:56 +0200

    it works

---
+++ ChargesSimulator/src/gui/CountAll.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class CountAll {
+	
+	public void Count(){
+	//System.out.println(stationaryChargesList.get(0).getX());
+	}
+
+}
---
+++ ChargesSimulator/src/gui/StationaryQ.java
@@ -0,0 +1,16 @@
+package gui;
+//rafdam
+
+import java.awt.Graphics;
+
+import model.ChargesList;
+import model.StationaryCharge;
+
+public class StationaryQ extends StationaryCharge{
+	
+    ChargesList stationaryChargesList = new ChargesList();
+    
+    
+    
+
+}
